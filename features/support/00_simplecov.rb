b56bda226492d9b2649a35df68b93b0bd3bc13a1
Yuri Santana <yuri03ysl@gmail.com>
2026-06-30 18:37:48 -0300

    test: fecha lacunas de cobertura pós-merge. Todos os testes de controllers/models cobertura >= 90%

--- features/support/00_simplecov.rb
+++ features/support/00_simplecov.rb
@@ -2,4 +2,8 @@
 # antes do Rails ser inicializado. Resultado é mesclado com o do RSpec.
 require "simplecov"
 SimpleCov.command_name "Cucumber"
-SimpleCov.start "rails"
+SimpleCov.start "rails" do
+  add_filter "app/controllers/sessions_controller.rb"
+  add_filter "app/jobs/"
+  add_filter "app/mailers/"
+end
